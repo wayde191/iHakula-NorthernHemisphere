e43489ba503150a1d5ef96dedd52cf95c1f5d921
CNwwsun <wwsun@thoughtworks.com>
2016-02-16 21:23:20 +0800

    Added : init for project; Wayde

---
+++ src/northern_hemisphere/core.clj
@@ -0,0 +1,7 @@
+(ns northern-hemisphere.core
+  (:gen-class))
+
+(defn -main
+  "I don't do a whole lot ... yet."
+  [& args]
+  (println "Hello, World!"))
---
+++ test/northern_hemisphere/core_test.clj
@@ -0,0 +1,7 @@
+(ns northern-hemisphere.core-test
+  (:require [clojure.test :refer :all]
+            [northern-hemisphere.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 1 1))))
