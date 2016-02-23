7a86effdb90a86364ca8c17e7250e40aea74ff48
CNwwsun <wwsun@thoughtworks.com>
2016-02-23 20:54:10 +0800

    Modified: set it to live; Wayde

--- go
+++ go
@@ -1,5 +1,5 @@
 #!/bin/bash -eu
 
-#. devenv
+. devenv
 
 lein "$@"
--- project.clj
+++ project.clj
@@ -4,36 +4,18 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.7.0"]
-                 [ring/ring-core "1.2.0"]
-                 [compojure "1.1.5"]
-                 [clj-http "1.0.1"]
+                 [compojure "1.1.6"]
+                 [clj-http "2.1.0"]
                  [clj-http-s3 "0.3.0" :exclusions [clj-http]]
-                 [clj-time "0.6.0"]
-                 [environ "0.4.0"]
-                 [org.clojars.scsibug/feedparser-clj "0.4.0"]
-                 [org.clojure/data.zip "0.1.1"]
-                 [org.clojure/data.json "0.2.3"]
-                 [org.clojure/data.csv "0.1.2"]
-                 [org.clojure/core.incubator "0.1.3"]
+                 [clj-time "0.11.0"]
                  [ring/ring-json "0.2.0"]
-                 [enlive "1.1.5"]
-                 [pjstadig/humane-test-output "0.6.0"]]
-  :injections [(require 'pjstadig.humane-test-output)
-               (pjstadig.humane-test-output/activate!)]
-  :plugins [[lein-ring "0.9.1"]
-            [lein-shell "0.4.0"]
-            [lein-rpm "0.0.5"]
-            [jonase/eastwood "0.1.4"]
-            [lein-cloverage "1.0.2"]]
+                 [enlive "1.1.6"]
+                 [environ "0.4.0"]
+                 ]
+  :plugins [[lein-ring "0.9.7"]]
   :ring {:handler northern-hemisphere.handler/app}
   :aot [utils.manifest]
-  :profiles {
-              :uberjar {:aot :all}
-              :dev {:dependencies [[ring-mock "0.1.5"]
-                                   [clj-http-fake "1.0.1"]
-                                   [ring-server "0.2.8"]
-                                   [clj-webdriver "0.6.1"]
-                                   [com.github.detro.ghostdriver/phantomjsdriver "1.1.0"]
-                                   [midje "1.5.1"]]}}
+  :profiles {:uberjar {:aot :all}}
   :main ^:skip-aot northern-hemisphere.core
-  :target-path "target/%s")
+  :target-path "target/%s"
+  :aliases {"clean-test" ["test"]})
