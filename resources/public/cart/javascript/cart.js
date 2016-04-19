b271052669e0b163f8cda96195e719603f025d16
CNwwsun <wwsun@thoughtworks.com>
2016-04-19 08:47:22 +0800

    init cart; Wayde

---
+++ resources/public/cart/javascript/cart.js
@@ -0,0 +1,21 @@
+var app = angular.module('p1-scorecard', ['ngRoute', 'ngResource', 'gofigure-common']);
+
+app.config(['$routeProvider', function($routeProvider) {
+    $routeProvider.
+        when('/', {
+            templateUrl: 'cart/partials/index.html',
+            controller: 'dashboardController',
+            reloadOnSearch: true
+        });
+}]);
+
+angular.bootstrap().invoke(bootstrap('p1-scorecard'));
+
+app.controller('dashboardController', function($scope) {
+    $scope.cart = 'cart/partials/cart.html';
+});
+
+app.controller('CartController', function($scope) {
+
+});
+
